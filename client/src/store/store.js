ffe158271ba46b47c50a65004ce50245cddaa33c
SayanthPavi <sayanthcpavi@gmail.com>
2025-02-18 09:48:54 +0530

    Created Shopping view account address crud and address slice

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,7 @@ import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice/index";
 import shopProductsSlice from "./shop/product-slice/index";
 import shopCartSlice from "./shop/cart-slice/index";
+import shopAddressSlice from "./shop/address-slice/index";
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     adminProducts: adminProductsSlice,
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
+    shopAddress: shopAddressSlice,
   },
 });
 
